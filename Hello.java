937de034372f849f4d59ec10049b70bb4020738c
dooleymm <46756648+dooleymm@users.noreply.github.com>
2019-01-16 12:14:16 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,25 @@
+/*****************************************************************
+*Hello.java
+*****************************************************************
+*Program 1/ Prints Student Info and Hello
+*________________________________________________________________
+*Maggie Dooley 
+*1/16/19
+*CMSC 255 and 004
+*****************************************************************/
+
+public class Hello {
+	public static void printHeading() {
+		System.out.println("Maggie Dooley");
+		System.out.println("Hello.java");
+		System.out.println("Prints Student Info and Hello");
+		System.out.println("1/16/19");
+		System.out.println("CMSC 255 and 004");
+		System.out.println();
+	}
+	
+	public static void main(String [] args) {
+		printHeading();
+		System.out.println("Hello");
+	}
+}

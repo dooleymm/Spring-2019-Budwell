ebc0a78611a40fc2714a060565ef601f86cb4b2c
dooleymm <46756648+dooleymm@users.noreply.github.com>
2019-01-23 13:11:18 -0500

    Add files via upload

---
+++ NameArt.java
@@ -0,0 +1,42 @@
+
+
+
+
+
+
+
+ /***********************************************************************************************
+*Project 1- Maggie
+*************************************************************************************************
+* Project Description:
+*This project outputs my name in a unique way on the display screen
+*Maggie Dooley
+*January 22, 2019
+*CMSC 255 Section 4
+*************************************************************************************************/
+public class NameArt{
+	public static void printHeading() {
+		System.out.println("Maggie Dooley");
+		System.out.println("NameArt.java");
+		System.out.println("This project outputs my name in a unique way on the display screen");
+		System.out.println("1/22/19");
+		System.out.println("CMSC 255 and 004");
+		System.out.println();
+	}
+	public static void main(String [] args) {
+		printHeading();
+System.out.println("**** * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * **** ");
+System.out.println(" *  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *  *  ");
+System.out.println("  *                                               *           *       *        *        *       *   *****                              *");
+System.out.println("   *                                              *  *     *  *      *  *    *   *    *   *     *   *                                 * ");
+System.out.println("  *                                               *    *  *   *     ******   *  ****  *  ****   *   *****                              *");
+System.out.println("*                                                 *     *    *    *       *  *     *  *     *   *   *                                  *");
+System.out.println(" *                                                *           *  *         * ******   ******    *   ******                            * ");
+System.out.println("  *                                                                                                                                  *  ");
+System.out.println("   *                                                                                                                                   *");
+System.out.println("  *                                                                                                                                   * ");
+System.out.println("*                                                                                                                                    *  ");
+System.out.println(" *  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *   * ");
+System.out.println("**** * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *  ****");
+	}
+}
